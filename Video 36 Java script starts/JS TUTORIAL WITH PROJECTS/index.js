2f8fae2f886431b2d67ca86e966c9f065a49c622
Samiullah Khan <kgamers748@gmail.com>
2026-02-06 23:56:22 +0530

    Added new folder day36 java first day

---
+++ Video 36 Java script starts/JS TUTORIAL WITH PROJECTS/index.js
@@ -0,0 +1,83 @@
+// 1. ways to print in JavaScript
+// console.log("Hello, World!");
+// alert("Welcome to this JavaScript Tutorial!");
+// document.write("This is a JavaScript Tutorial.");
+
+// 2. JavaScript console API
+// console.log("Hello, World!");
+// console.warn("This is a warning message.");
+// console.error("This is an error message.");
+
+// 3. JavaScript variables
+// var name = "John";
+// let age = 30;
+// const pi = 3.14;
+// 4. Data types in JavaScript
+// let name = "John"; // String
+// var str1 = 'This is a string using single quotes.';
+// var str2 = "This is a string using double quotes.";
+// var str3 = `This is a string using backticks.`;
+// let age = 30; // Number
+// var num1 = 45.5; // This is a floating-point number
+
+// objects
+// var marks = {
+//     math: 95,
+//     science: 90,
+//     english: 85
+// }
+
+// console.log(marks);
+
+// boolean in javaScript
+// let isStudent = true;
+// let isTeacher = false;
+// console.log(isStudent, isTeacher);
+
+// var und; // undefined
+// console.log(und);
+// var n = null; // null
+// console.log(n);
+
+// At a very high level, there are two types of data types in javaScript:
+// 1. Primitive data types: undefined, null, number, string, boolean, symbol
+// 2. Reference data types: arrays and objects
+
+
+// var arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+// operators in JavaScript
+// Arithmetic operators
+// let a = 10;
+// let b = 5;
+// console.log("a + b =", a + b); // Addition
+// console.log("a - b =", a - b); // Subtraction
+// console.log("a * b =", a * b); // Multiplication
+// console.log("a / b =", a / b); // Division
+// console.log("a % b =", a % b); // Modulus
+// Assignment operators
+// let c = 10;
+// c += 5; // c = c + 5
+// console.log("c =", c);
+// c -= 3; // c = c - 3
+// console.log("c =", c);
+// c *= 2; // c = c * 2
+// console.log("c =", c);
+// c /= 4; // c = c / 4
+// console.log("c =", c);
+// Comparison operators
+// let x = 10;
+// let y = 20;
+// console.log("x == y:", x == y); // Equal to
+// console.log("x != y:", x != y); // Not equal to
+// console.log("x > y:", x > y); // Greater than
+// console.log("x < y:", x < y); // Less than
+// console.log("x >= y:", x >= y); // Greater than or equal to
+// console.log("x <= y:", x <= y); // Less than or equal to
+// Logical operators
+// let p = true;
+// let q = false;
+// console.log("p && q:", p && q); // Logical AND
+// console.log("p || q:", p || q); // Logical OR
+// console.log("!p:", !p); // Logical NOT
