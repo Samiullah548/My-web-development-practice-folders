30ee4cdf443a37b1902c0e82c09a0e56a5b6a2a6
Samiullah Khan <kgamers748@gmail.com>
2026-02-10 21:35:21 +0530

    Dates in Java Script

---
+++ Video 40 js/scripty.js
@@ -0,0 +1,19 @@
+// String Methods in JavaScript
+
+let myLovelyString = "Harry is a good boy good good";
+
+// console.log(myLovelyString.length); // Length of the string
+// console.log(myLovelyString.indexOf("good")); // Index of the substring "good" it will print 11 index.
+// console.log(myLovelyString.lastIndexOf("good")); // Last index of the substring "good"
+
+
+// console.log(myLovelyString.slice(0, 4)); // It will print "Harry" because it will start from index 0 and end at index 5 (not included).
+
+d = myLovelyString.replace("Harry", "Rohan"); // It will replace "Harry" with "Rohan" and return the new string.
+console.log(d, myLovelyString); // It will print the new string and the original string. The original string will remain unchanged because strings are immutable in JavaScript.
+
+// dates in java script
+
+var myDate = new Date(); // It will create a new date object with the current date and time.
+console.log(myDate); // It will print the current date and time.
+
