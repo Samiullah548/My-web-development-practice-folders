0fa5db4b6562591e22a7c061eb45fc5f03079b16
Samiullah Khan <kgamers748@gmail.com>
2026-02-11 18:41:53 +0530

    DOM BASICS

---
+++ Video 41 js DOM Manupulation/script.js
@@ -0,0 +1,11 @@
+// Maine js me padha
+// Window object 
+// What is dom?
+// When a web page is loaded, the browser creates a Document Object Model (DOM) of the page 
+// Jo hamara html ka code hota hai vo window document me store hota hai and the document folder is available in the window object 
+// ham console me agar window.document likh kar enter karen to document wala folder pura print ho jayega.
+// Aur agar ham 
+// console.dir(window.document); karen to ham properties dekh sakte hai document ki.
+// we prefer this console.dir to print document object.
+
+// Ek tree hota hai iska like
