6b0b6c89c65d56cd63e37fd68116f9f56bd22a7e
Samiullah Khan <kgamers748@gmail.com>
2026-03-02 21:55:29 +0530

    Practice sets

---
+++ Video 47 practice sets dom/script.js
@@ -0,0 +1,16 @@
+// practice set 1
+
+// newBtn = document.createElement("button");
+// newBtn.innerText = "CLick Me";
+
+// newBtn.style.color = "white";
+// newBtn.style.backgroundColor = "blue";
+// newBtn.style.padding = "10px 20px";
+// newBtn.style.border = "none";
+// newBtn.style.borderRadius = "5px";
+// newBtn.style.cursor = "pointer";
+
+// document.querySelector("body").prepend(newBtn);
+
+// practice set 2
+
