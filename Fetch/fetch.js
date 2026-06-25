c8fcb0c747614aafdc466e22589f17a5c2a1160f
Samiullah Khan <kgamers748@gmail.com>
2026-06-25 13:21:27 +0530

    Fetch

---
+++ Fetch/fetch.js
@@ -0,0 +1,1 @@
+// A fetch() promise only rejects when a network error is encountered (which is usually when there's a permissions issue or similar). A fetch() promise does not reject on HTTP error (404, etc). Instead, a then() Handler must check the Response.ok and/or Response.status properties.
