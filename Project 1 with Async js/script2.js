c0d47f3afd95f3e901565abf79b9f5386b45a239
Samiullah Khan <kgamers748@gmail.com>
2026-06-25 13:11:26 +0530

    Async js project

---
+++ Project 1 with Async js/script2.js
@@ -0,0 +1,19 @@
+console.log("This is project 2");
+window.addEventListener("keydown", function(e) {
+    insert.innerHTML = `
+    <div class="color">
+     <table>
+  <tr>
+    <th>Key</th>
+    <th>Key Code</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key == '' ? 'Space' : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    </div>
+    `
+})
